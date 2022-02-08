bddc1e30a9b943010d21e0d41c0c57afac023081
Solomon <solo.serry@gmail.com>
2022-02-08 12:39:23 -0500

    added pseudo code

--- src/App.js
+++ src/App.js
@@ -13,3 +13,23 @@ function App() {
 }
 
 export default App;
+
+
+// Create form for users search choices
+  // -submit on form
+  // -on change and value on input field 
+
+// Users login area 
+
+// Create feature that saves users song choice to personal playlist via firebase
+
+// possibly use node.js instead of firebase 
+
+// 2 search features, one for artist and one for song title 
+  // - which ever search feature user decides to use the other will be blocked 
+
+// second feature: this will allow user to search any random word and get back music based on that word
+
+// add react router dom 
+
+
