c70e1b9654df38c2489c74557e0a5bcca11fe385
Solomon <solo.serry@gmail.com>
2022-03-08 13:35:06 -0500

    added sign in and sign up modal

--- src/componets /Modal.js
+++ src/componets /Modal.js
@@ -1,17 +1,24 @@
 import { useSpring, animated } from "react-spring"
+import { useState } from "react"
 
 const Modal = ({ showModal, setShowModal }) => {
 
-    const handleLogin = () => {
+    const [logInToggle, setLogInToggle] = useState()
+
+    const handleLogIn = () => {
         setShowModal(false)
     }
+
+    // const handleSignIn = () => {
+    //     setLogInToggle(!logInToggle)
+    // }
 
     const animate = useSpring({
         config: {
             duration: 250
         },
         opacity: showModal ? 1 : 0,
-        transform: showModal ? `translate(0%)` : `translate(-100%)`
+        transform: showModal ? `translateY(0%)` : `translateY(-100%)`
     });
 
 
@@ -24,14 +31,51 @@ const Modal = ({ showModal, setShowModal }) => {
 
                 <animated.div style={animate}>
                     <div className="modalContent">
-                        <form onSubmit={handleLogin}>
-                            <label className="sr-only" htmlFor="userName"> User Name </label>
-                            <input placeholder="User Name" type="text" id="name" />
+                        <form onSubmit={handleLogIn}>
+                            {!logInToggle ? 
 
-                            <label className="sr-only" htmlFor="userEmail"> User Email </label>
-                            <input placeholder="User Email" type="email" id="email" />
-                            
-                            <button> Enter </button>
+                            <>
+                                 <div className="signInToggle">
+                                    <p onClick={() => setLogInToggle(false)}>Sign In</p>
+                                    <p onClick={() => setLogInToggle(true)}>Sign Up</p>
+                                </div>
+
+                                <h2>Sign In</h2>
+
+                                <label className="sr-only" htmlFor="userEmail"> Email </label>
+                                <input placeholder="Email" type="email" id="email" />
+
+                                <label className="sr-only" htmlFor="userPassword"> Password </label>
+                                <input placeholder="Password" type="passsword" id="password" />
+
+                                <button> Enter </button>
+
+                                <p onClick={handleLogIn}>Continue As Guest</p>
+                            </>
+
+                            : 
+                            <>
+                                <div className="signUpToggle">
+                                    <p onClick={() => setLogInToggle(false)}>Sign In</p>
+                                    <p onClick={() => setLogInToggle(true)}>Sign Up</p>
+                                </div>
+
+                                <h2>Sign Up</h2>
+
+                                <label className="sr-only" htmlFor="userName"> User Name </label>
+                                <input placeholder="User Name" type="text" id="name" />
+
+                                <label className="sr-only" htmlFor="userEmail"> Email </label>
+                                <input placeholder="Email" type="email" id="email" />
+
+                                <label className="sr-only" htmlFor="userPassword"> Password </label>
+                                <input placeholder="Password" type="passsword" id="password" />
+
+                                <button > Enter </button>
+
+                                <p onClick={handleLogIn}>Continue As Guest</p>
+                            </>
+                            }
                         </form>
                     </div>
                 </animated.div>
